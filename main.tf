7694fcb7a31a960d6f375f5f8936efec75beeb03
Sarkis Varozian <svarozian@gmail.com>
2018-06-14 07:44:13 -0700

    use length to get number of members in list

--- main.tf
+++ main.tf
@@ -42,7 +42,7 @@ resource "aws_lb_target_group" "default" {
 }
 
 resource "aws_lb_listener_rule" "default" {
-  count        = "${count(var.listener_arns)}"
+  count        = "${length(var.listener_arns)}"
   listener_arn = "${var.listener_arns[count.index]}"
   priority     = "${var.priority+var.listener_arns[count.index]}"
 
