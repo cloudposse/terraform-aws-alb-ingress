339741de6f308c309f9e285cc553033e78485129
Andrew Mackett <andrewm@yoyowallet.com>
2019-07-24 14:31:24 +0100

    convert with terraform 0.12upgrade command

--- outputs.tf
+++ outputs.tf
@@ -1,14 +1,15 @@
 output "target_group_name" {
   description = "ALB Target group name"
-  value       = "${data.aws_lb_target_group.default.name}"
+  value       = data.aws_lb_target_group.default.name
 }
 
 output "target_group_arn" {
   description = "ALB Target group ARN"
-  value       = "${data.aws_lb_target_group.default.arn}"
+  value       = data.aws_lb_target_group.default.arn
 }
 
 output "target_group_arn_suffix" {
   description = "ALB Target group ARN suffix"
-  value       = "${data.aws_lb_target_group.default.arn_suffix}"
+  value       = data.aws_lb_target_group.default.arn_suffix
 }
+
--- variables.tf
+++ variables.tf
@@ -1,222 +1,222 @@
 variable "namespace" {
-  type        = "string"
+  type        = string
   description = "Namespace, which could be your organization name, e.g. `cp` or `cloudposse`"
 }
 
 variable "stage" {
-  type        = "string"
+  type        = string
   description = "Stage, e.g. `prod`, `staging`, `dev`, or `test`"
 }
 
 variable "name" {
-  type        = "string"
+  type        = string
   description = "Solution name, e.g. `app`"
 }
 
 variable "delimiter" {
-  type        = "string"
+  type        = string
   default     = "-"
   description = "Delimiter to be used between `namespace`, `name`, `stage` and `attributes`"
 }
 
 variable "attributes" {
-  type        = "list"
+  type        = list(string)
   default     = []
   description = "Additional attributes, e.g. `1`"
 }
 
 variable "tags" {
-  type        = "map"
+  type        = map(string)
   default     = {}
   description = "Additional tags (e.g. `map(`BusinessUnit`,`XYZ`)"
 }
 
 variable "target_group_arn" {
-  type        = "string"
+  type        = string
   default     = ""
   description = "ALB target group ARN. If this is an empty string, a new one will be generated"
 }
 
 variable "unauthenticated_listener_arns" {
-  type        = "list"
+  type        = list(string)
   default     = []
   description = "A list of unauthenticated ALB listener ARNs to attach ALB listener rules to"
 }
 
 variable "unauthenticated_listener_arns_count" {
-  type        = "string"
-  default     = "0"
+  type        = number
+  default     = 0
   description = "The number of unauthenticated ARNs in `unauthenticated_listener_arns`. This is necessary to work around a limitation in Terraform where counts cannot be computed"
 }
 
 variable "authenticated_listener_arns" {
-  type        = "list"
+  type        = list(string)
   default     = []
   description = "A list of authenticated ALB listener ARNs to attach ALB listener rules to"
 }
 
 variable "authenticated_listener_arns_count" {
-  type        = "string"
-  default     = "0"
+  type        = number
+  default     = 0
   description = "The number of authenticated ARNs in `authenticated_listener_arns`. This is necessary to work around a limitation in Terraform where counts cannot be computed"
 }
 
 variable "deregistration_delay" {
-  type        = "string"
-  default     = "15"
+  type        = number
+  default     = 15
   description = "The amount of time to wait in seconds while deregistering target"
 }
 
 variable "health_check_path" {
-  type        = "string"
+  type        = string
   default     = "/"
   description = "The destination for the health check request"
 }
 
 variable "health_check_timeout" {
-  type        = "string"
-  default     = "10"
+  type        = number
+  default     = 10
   description = "The amount of time to wait in seconds before failing a health check request"
 }
 
 variable "health_check_healthy_threshold" {
-  type        = "string"
-  default     = "2"
+  type        = number
+  default     = 2
   description = "The number of consecutive health checks successes required before healthy"
 }
 
 variable "health_check_unhealthy_threshold" {
-  type        = "string"
-  default     = "2"
+  type        = number
+  default     = 2
   description = "The number of consecutive health check failures required before unhealthy"
 }
 
 variable "health_check_interval" {
-  type        = "string"
-  default     = "15"
+  type        = number
+  default     = 15
   description = "The duration in seconds in between health checks"
 }
 
 variable "health_check_matcher" {
-  type        = "string"
+  type        = string
   default     = "200-399"
   description = "The HTTP response codes to indicate a healthy check"
 }
 
 variable "unauthenticated_priority" {
-  type        = "string"
-  default     = "100"
+  type        = number
+  default     = 100
   description = "The priority for the rules without authentication, between 1 and 50000 (1 being highest priority). Must be different from `authenticated_priority` since a listener can't have multiple rules with the same priority"
 }
 
 variable "authenticated_priority" {
-  type        = "string"
-  default     = "300"
+  type        = number
+  default     = 300
   description = "The priority for the rules with authentication, between 1 and 50000 (1 being highest priority). Must be different from `unauthenticated_priority` since a listener can't have multiple rules with the same priority"
 }
 
 variable "port" {
-  type        = "string"
-  default     = "80"
+  type        = number
+  default     = 80
   description = "The port for generated ALB target group (if `target_group_arn` not set)"
 }
 
 variable "protocol" {
-  type        = "string"
+  type        = string
   default     = "HTTP"
   description = "The protocol for generated ALB target group (if `target_group_arn` not set)"
 }
 
 variable "target_type" {
-  type    = "string"
+  type    = string
   default = "ip"
 }
 
 variable "vpc_id" {
-  type        = "string"
+  type        = string
   description = "The VPC ID where generated ALB target group will be provisioned (if `target_group_arn` is not set)"
 }
 
 variable "unauthenticated_hosts" {
-  type        = "list"
+  type        = list(string)
   default     = []
   description = "Unauthenticated hosts to match in Hosts header"
 }
 
 variable "authenticated_hosts" {
-  type        = "list"
+  type        = list(string)
   default     = []
   description = "Authenticated hosts to match in Hosts header"
 }
 
 variable "unauthenticated_paths" {
-  type        = "list"
+  type        = list(string)
   default     = []
   description = "Unauthenticated path pattern to match (a maximum of 1 can be defined)"
 }
 
 variable "authenticated_paths" {
-  type        = "list"
+  type        = list(string)
   default     = []
   description = "Authenticated path pattern to match (a maximum of 1 can be defined)"
 }
 
 variable "authentication_type" {
-  type        = "string"
+  type        = string
   default     = ""
   description = "Authentication type. Supported values are `COGNITO` and `OIDC`"
 }
 
 variable "authentication_cognito_user_pool_arn" {
-  type        = "string"
+  type        = string
   description = "Cognito User Pool ARN"
   default     = ""
 }
 
 variable "authentication_cognito_user_pool_client_id" {
-  type        = "string"
+  type        = string
   description = "Cognito User Pool Client ID"
   default     = ""
 }
 
 variable "authentication_cognito_user_pool_domain" {
-  type        = "string"
+  type        = string
   description = "Cognito User Pool Domain. The User Pool Domain should be set to the domain prefix (`xxx`) instead of full domain (https://xxx.auth.us-west-2.amazoncognito.com)"
   default     = ""
 }
 
 variable "authentication_oidc_client_id" {
-  type        = "string"
+  type        = string
   description = "OIDC Client ID"
   default     = ""
 }
 
 variable "authentication_oidc_client_secret" {
-  type        = "string"
+  type        = string
   description = "OIDC Client Secret"
   default     = ""
 }
 
 variable "authentication_oidc_issuer" {
-  type        = "string"
+  type        = string
   description = "OIDC Issuer"
   default     = ""
 }
 
 variable "authentication_oidc_authorization_endpoint" {
-  type        = "string"
+  type        = string
   description = "OIDC Authorization Endpoint"
   default     = ""
 }
 
 variable "authentication_oidc_token_endpoint" {
-  type        = "string"
+  type        = string
   description = "OIDC Token Endpoint"
   default     = ""
 }
 
 variable "authentication_oidc_user_info_endpoint" {
-  type        = "string"
+  type        = string
   description = "OIDC User Info Endpoint"
   default     = ""
 }
