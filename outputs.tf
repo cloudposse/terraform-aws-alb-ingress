4929c22ee0be394d8124b5a6d643c0ffebfd32d0
nitro <nitrocode@users.noreply.github.com>
2020-06-19 09:27:44 -0400

    Add enabled variable (#27)
    * Add enabled variable
    
    * Updated README.md
    
    * Added test from terraform-aws-alb
    
    * Update fixtures.us-east-2.tfvars
    
    * Update examples_complete_test.go
    
    Co-authored-by: Erik Osterman <erik@cloudposse.com>
    Co-authored-by: actions-bot <58130806+actions-bot@users.noreply.github.com>

--- outputs.tf
+++ outputs.tf
@@ -1,14 +1,14 @@
 output "target_group_name" {
   description = "ALB Target Group name"
-  value       = data.aws_lb_target_group.default.name
+  value       = join("", data.aws_lb_target_group.default.*.name)
 }
 
 output "target_group_arn" {
   description = "ALB Target Group ARN"
-  value       = data.aws_lb_target_group.default.arn
+  value       = join("", data.aws_lb_target_group.default.*.arn)
 }
 
 output "target_group_arn_suffix" {
   description = "ALB Target Group ARN suffix"
-  value       = data.aws_lb_target_group.default.arn_suffix
+  value       = join("", data.aws_lb_target_group.default.*.arn_suffix)
 }
